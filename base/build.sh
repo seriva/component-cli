69f2c9c07e6926fbdd6978dea0bd40090be58834
Luuk van Venrooij <luukvanvenrooij84@gmail.com>
2020-07-24 10:42:41 +0200

    Change for publishing docker images.

--- base/build.sh
+++ base/build.sh
@@ -1,2 +1,2 @@
 read E_VERSION < version
-docker build -t e:$E_VERSION -f Dockerfile . --build-arg E_BASE_VERSION=$E_VERSION
+docker build -t luukvv/base:$E_VERSION -f Dockerfile . --build-arg E_BASE_VERSION=$E_VERSION
